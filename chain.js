1c567efe801273831ab4b378f08271150f0862bb
Aishwary200 <72340367+Aishwary200@users.noreply.github.com>
2020-11-05 00:02:37 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,29 @@
+const Constraint= Matter.Constraint;
+class Chain{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB;
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+     fly(){
+  this.chain.bodyA=null;
+}
+    display(){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.pointB;
+        push();
+        stroke(48,22,8)
+        if(this.chain.bodyA){
+        
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
